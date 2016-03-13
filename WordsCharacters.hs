9d006cc3b10114e035ad8d8277c81e072921c24a
Dr Bean <drbean@localhost.localdomain>
2016-03-13 16:52:17 +0800

    barebones.removed_app_specific.Adv,AP,Det,N,PN,Prep,V.TODO

---
+++ WordsCharacters.hs
@@ -0,0 +1,125 @@
+module WordsCharacters where
+
+import qualified Data.Map as Map
+
+gfWords = Map.fromList [
+	("A",a)
+	, ("ADV",adv)
+	, ("Aux",aux)
+	, ("CONJ",conj)
+	, ("Det",det)
+	, ("N",n)
+	, ("PN",pn)
+	, ("Pron",pron)
+	, ("Prep",prep)
+	, ("Rel",rel)
+	, ("Tag",tag)
+	, ("V",v) ]
+
+wordlist = concat ( map (gfWords Map.!) (Map.keys gfWords) )
+
+posMap = Map.fromList [
+	("A","Adjective")
+	, ("ADV","Adverb")
+	, ("Aux","Auxiliary")
+	, ("CONJ","Conjunction")
+	, ("Det","Determiner")
+	, ("N","Noun")
+	, ("PN","Proper Noun")
+	, ("Pron","Pronoun")
+	, ("Prep","Preposition")
+	, ("Rel","Relative Pronoun")
+	, ("Tag","Question Tag")
+	, ("V","Verb")
+	]
+
+a = [
+
+	]
+
+adv = [
+
+	]
+
+aux = [
+	"doesn't"
+	, "don't"
+	, "does"
+	, "do"
+	, "is"
+	, "are"
+	, "isn't"
+	, "aren't"
+	, "would"
+	, "should"
+	]
+	
+
+conj = [
+	"because"
+	, "if"
+	]
+
+
+det = [
+	"'s"
+	, "0, _ or zero"
+	, "a"
+	, "an"
+	, "no"
+	, "some"
+	, "the"
+
+n = [
+
+	]
+
+pn = [
+
+	]
+
+pron = [
+	"who"
+	, "she"
+	, "her"
+	, "he"
+	, "his"
+	]
+
+prep = [
+
+	]
+
+rel = [
+	"that"
+	, "who"
+	]
+
+tag = [
+	"doesn't he"
+	, "doesn't she"
+	, "doesn't it"
+	, "don't they"
+	, "does he"
+	, "does she"
+	, "does it"
+	, "do they"
+	, "isn't he"
+	, "isn't she"
+	, "isn't it"
+	, "aren't they"
+	, "is he"
+	, "is she"
+	, "is it"
+	, "are they"
+	]
+
+v = [
+
+	]
+
+{-
+
+-}
+
+-- vim: set ts=2 sts=2 sw=2 noet:
